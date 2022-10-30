5277b79cb7c44bd23137b5c683fa6e9b6e5d21a8
neJustbear <114687589+neJustbear@users.noreply.github.com>
2022-10-30 22:35:31 +0300

    Add files via upload

---
+++ ConsoleApplication2/ConsoleApplication2/ConsoleApplication2.cpp
@@ -0,0 +1,35 @@
+﻿#include <iostream>
+#include <string>
+#include <fstream>
+#include <windows.h>
+#include <regex>
+
+void is_valid_hyperlink(const std::string& str) {
+    static const std::regex r(R"(([\w+]+\:\/\/)?([\w\d-]+\.)*[\w-]+[\.\:]\w+([\/\?\=\&\#\.]?[\w-]+)*\/?)");
+    std::smatch match;
+    if (regex_search(str, match, r))
+        std::cout << match[0] << std::endl;
+}
+
+
+int main()
+{
+    setlocale(LC_ALL, "ru");
+
+    std::ifstream fin;
+    //std::string file;
+    //std::cin >> file;
+    fin.open("test.html");
+    if (!fin)
+        std::cout << "Ошибка открытия файла\n\n";
+    else
+        std::cout << "Файл открыт\n\n";
+
+    std::string str;
+    while (std::getline(fin, str)) {
+        is_valid_hyperlink(str);
+    }
+
+    return 0;
+
+}
